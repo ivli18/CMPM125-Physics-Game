4423b98c0b702423a7ccfc214d777dbb2dcdd1ea
zSinani <ili18@ucsc.edu>
2026-04-23 01:39:53 -0700

    Minor cleanup
    Failed at adding some sort of drag

--- My project/Assets/Scripts/GameManager.cs
+++ My project/Assets/Scripts/GameManager.cs
@@ -1,19 +1,28 @@
 using TMPro;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.Android;
 using UnityEngine.InputSystem;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 public class GameManager : MonoBehaviour
 {
     public static GameManager Instance;
+    public GameObject player;
+    private Rigidbody playerObject;
     public InputAction moveAction;
     public Transform level;
-    public float tiltSpeed = 5f;
-    // Public for testing, doesn't need to be public in final version
-    public float maxTilt = 20f;
+    private float tiltSpeed = 3f;
+    private float maxTilt = 20f;
+    private float groundForce = 10f;
+    private float assistForce = 15f;
+    private float normalFriction = 20f;
+    private float iceFriction = 0.1f;
     public TextMeshProUGUI timerText;
-    public int score = 0;
+    private int score = 0;
     public TextMeshProUGUI scoreText;
+    private Vector2 input;
+
     private void Awake()
     {
         Instance = this;
@@ -31,23 +40,63 @@ public class GameManager : MonoBehaviour
     void Start()
     {
         OnEnable();
+        playerObject = player.GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        Vector2 input = moveAction.ReadValue<Vector2>();
-        float tiltX = input.y * maxTilt;
-        float tiltZ = -input.x * maxTilt;
-        Quaternion targetRotation = Quaternion.Euler(tiltX, 0, tiltZ);
-        level.rotation = Quaternion.Slerp(level.rotation, targetRotation, Time.deltaTime * tiltSpeed);
+        input = moveAction.ReadValue<Vector2>();
         // Update timer text
         float time = Time.timeSinceLevelLoad;
         timerText.text = time.ToString("F2") + "s";
         scoreText.text = "Score: " + score;
     }
+    void FixedUpdate()
+    {
+        // // Material detection
+        // RaycastHit hit;
+        // bool grounded = Physics.Raycast(transform.position, Vector3.down, 1.2f);
+        // if (Physics.Raycast(transform.position, Vector3.down, out hit, 1.2f))
+        // {
+        //     if (hit.collider.sharedMaterial != null)
+        //     {
+        //         Debug.Log(hit.collider.sharedMaterial.name);
+        //     }
+        // }
+        // // Material handling
+        // if (grounded)
+        // {
+        //     if (hit.collider.sharedMaterial != null && hit.collider.sharedMaterial.name == "Ice Physics Material")
+        //     {
+        //         playerObject.linearDamping = iceFriction;
+        //     }
+        //     else
+        //     {
+        //         playerObject.linearDamping = normalFriction;
+        //     }
+        // }
+        // else
+        // {
+        //     playerObject.linearDamping = 0f;
+        // }
+
+        // Level tilting
+        float tiltX = input.y * maxTilt;
+        float tiltZ = -input.x * maxTilt;
+        Quaternion targetRotation = Quaternion.Euler(tiltX, 0, tiltZ);
+        level.rotation = Quaternion.Slerp(level.rotation, targetRotation, Time.fixedDeltaTime * tiltSpeed);
+        // Ball movement asisstance
+        Vector3 tiltDir = Vector3.ProjectOnPlane(Vector3.down, level.up).normalized;
+        playerObject.AddForce(Vector3.down * groundForce, ForceMode.Acceleration);
+        if (input != Vector2.zero)
+        {
+            playerObject.AddForce(tiltDir * assistForce, ForceMode.Acceleration);
+        }
+    }
     public void AddScore(int amount)
     {
         score += amount;
     }
+
 }
