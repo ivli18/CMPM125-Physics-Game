6bed27e808fbd10a531db5b94e0cfab6f13660e1
Bryce <bhan16@ucsc.edu>
2026-04-23 21:46:10 -0700

    Added LVL1

--- My project/Assets/Scripts/WinArea.cs
+++ My project/Assets/Scripts/WinArea.cs
@@ -3,6 +3,7 @@ using UnityEngine.SceneManagement;
 
 public class WinArea : MonoBehaviour
 {
+    [SerializeField] private string levelToLoad; // The name of the level to load when the player wins
     // Win area script
     // This script can be attached to objects that should cause the player to win when they collide with them.
 
@@ -12,7 +13,7 @@ public class WinArea : MonoBehaviour
         // Check if the colliding object is the player
         if (other.gameObject.CompareTag("Player"))
         {
-            SceneManager.LoadScene("Level1");
+            SceneManager.LoadScene(levelToLoad);
         }
     }
 }
